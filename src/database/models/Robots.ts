06f01cd43d29886bb0ab03666e9a9a8a6f9f4046
Andrea Prat <andr8ide@gmail.com>
2023-08-26 10:35:33 +0200

    Create robotSchema and Robot model

---
+++ src/database/models/Robots.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import type RobotStructure from "./Types.js";
+
+const robotSchema = new Schema<RobotStructure>({
+  name: {
+    type: String,
+    requiered: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  speed: {
+    type: String,
+    requiered: true,
+  },
+  endurance: {
+    type: String,
+    requiered: true,
+  },
+});
+
+const Robot = model("Robots", robotSchema, "robots");
+
+export default Robot;
